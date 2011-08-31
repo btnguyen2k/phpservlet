8c2dc9b4d448b7d2bef29539c8187051e5cb1958
btnguyen2k <btnguyen2k@0f6370a9-534a-651e-4768-c0c393c7bce9>
2011-08-31 15:23:42 +0000

    * Final release v0.1

--- src/org/ddth/phpservlet/PhpFilter.java
+++ src/org/ddth/phpservlet/PhpFilter.java
@@ -22,126 +22,120 @@ import com.google.common.collect.MapMaker;
 
 public class PhpFilter implements Filter {
 
-	private final static String PARAM_NAME = "quercusServletName";
-	private String quercusServletName = "QuercusServlet";
-	private String contextPath;
-	private ServletContext servletContext;
-	private ConcurrentMap<String, Boolean> cache;
-	private ConcurrentMap<String, PhpScriptInfo> cache2;
+    private final static String PARAM_NAME = "quercusServletName";
+    private String quercusServletName = "QuercusServlet";
+    private String contextPath;
+    private ServletContext servletContext;
+    private ConcurrentMap<String, Boolean> cache;
+    private ConcurrentMap<String, PhpScriptInfo> cache2;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void destroy() {
-		if (cache != null) {
-			cache.clear();
-			cache = null;
-		}
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void destroy() {
+        if (cache != null) {
+            cache.clear();
+            cache = null;
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void init(FilterConfig config) throws ServletException {
-		servletContext = config.getServletContext();
-		contextPath = servletContext.getContextPath();
-		String quercusServletName = config.getInitParameter(PARAM_NAME);
-		if (quercusServletName != null) {
-			this.quercusServletName = quercusServletName;
-		}
-		cache = new MapMaker().concurrencyLevel(4).maximumSize(1024)
-				.expireAfterWrite(5, TimeUnit.MINUTES).makeMap();
-		cache2 = new MapMaker().concurrencyLevel(4).maximumSize(10240)
-				.expireAfterWrite(5, TimeUnit.MINUTES).makeMap();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        servletContext = config.getServletContext();
+        contextPath = servletContext.getContextPath();
+        String quercusServletName = config.getInitParameter(PARAM_NAME);
+        if (quercusServletName != null) {
+            this.quercusServletName = quercusServletName;
+        }
+        cache = new MapMaker().concurrencyLevel(4).maximumSize(1024).expireAfterWrite(5,
+                TimeUnit.MINUTES).makeMap();
+        cache2 = new MapMaker().concurrencyLevel(4).maximumSize(10240).expireAfterWrite(5,
+                TimeUnit.MINUTES).makeMap();
+    }
 
-	private PhpScriptInfo buildLevel2CacheEntry(String resourcePath,
-			String pathInfo) {
-		PhpScriptInfo phpScriptInfo = new PhpScriptInfo(resourcePath, pathInfo);
-		cache2.put(resourcePath, phpScriptInfo);
-		return phpScriptInfo;
-	}
+    private PhpScriptInfo buildLevel2CacheEntry(String resourcePath, String pathInfo) {
+        PhpScriptInfo phpScriptInfo = new PhpScriptInfo(resourcePath, pathInfo);
+        cache2.put(resourcePath, phpScriptInfo);
+        return phpScriptInfo;
+    }
 
-	/**
-	 * Checks if the current request invokes a PHP script.
-	 * 
-	 * @param httpRequest
-	 *            HttpServletRequest
-	 * @return PhpScriptInfo if the request invokes a PHP script,
-	 *         <code>null</code> otherwises
-	 * @throws MalformedURLException
-	 */
-	protected PhpScriptInfo isPhpRequest(HttpServletRequest httpRequest)
-			throws MalformedURLException {
-		String requestUri = httpRequest.getRequestURI();
-		// remove the context path
-		String resourcePath = StringUtils.substringAfter(requestUri,
-				contextPath);
+    /**
+     * Checks if the current request invokes a PHP script.
+     * 
+     * @param httpRequest
+     *            HttpServletRequest
+     * @return PhpScriptInfo if the request invokes a PHP script,
+     *         <code>null</code> otherwises
+     * @throws MalformedURLException
+     */
+    protected PhpScriptInfo isPhpRequest(HttpServletRequest httpRequest)
+            throws MalformedURLException {
+        String requestUri = httpRequest.getRequestURI();
+        // remove the context path
+        String resourcePath = StringUtils.substringAfter(requestUri, contextPath);
 
-		// check level 2 cache first
-		PhpScriptInfo cacheValue2 = cache2.get(resourcePath);
-		if (cacheValue2 != null) {
-			return cacheValue2;
-		}
+        // check level 2 cache first
+        PhpScriptInfo cacheValue2 = cache2.get(resourcePath);
+        if (cacheValue2 != null) {
+            return cacheValue2;
+        }
 
-		String[] tokens = StringUtils.split(resourcePath, '/');
-		// check longest path first!
-		int index = tokens.length - 1;
-		do {
-			String pathInfo = StringUtils.substringAfter(requestUri,
-					resourcePath);
-			Boolean cacheValue = cache.get(resourcePath);
-			if (cacheValue != null) {
-				return cacheValue ? buildLevel2CacheEntry(resourcePath,
-						pathInfo) : null;
-			}
-			URL resource = servletContext.getResource(resourcePath);
-			if (resource == null) {
-				String token = "/" + tokens[index];
-				resourcePath = StringUtils.substringBeforeLast(resourcePath,
-						token);
-				// continue to check
-				index--;
-				continue;
-			}
-			// the request should invoke a resource
-			if (resourcePath.toLowerCase().endsWith(".php")) {
-				// it's PHP script
-				cacheValue = Boolean.TRUE;
-			} else {
-				// it's not
-				cacheValue = Boolean.FALSE;
-			}
-			cache.put(resourcePath, cacheValue);
-			return cacheValue ? buildLevel2CacheEntry(resourcePath, pathInfo)
-					: null;
-		} while (index >= 0);
-		return null;
-	}
+        String[] tokens = StringUtils.split(resourcePath, '/');
+        // check longest path first!
+        int index = tokens.length - 1;
+        do {
+            String pathInfo = StringUtils.substringAfter(requestUri, resourcePath);
+            Boolean cacheValue = cache.get(resourcePath);
+            if (cacheValue != null) {
+                return cacheValue ? buildLevel2CacheEntry(resourcePath, pathInfo) : null;
+            }
+            URL resource = servletContext.getResource(resourcePath);
+            if (resource == null) {
+                String token = "/" + tokens[index];
+                resourcePath = StringUtils.substringBeforeLast(resourcePath, token);
+                // continue to check
+                index--;
+                continue;
+            }
+            // the request should invoke a resource
+            if (resourcePath.toLowerCase().endsWith(".php")) {
+                // it's PHP script
+                cacheValue = Boolean.TRUE;
+            } else {
+                // it's not
+                cacheValue = Boolean.FALSE;
+            }
+            cache.put(resourcePath, cacheValue);
+            return cacheValue ? buildLevel2CacheEntry(resourcePath, pathInfo) : null;
+        } while (index >= 0);
+        return null;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws ServletException, IOException {
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		PhpScriptInfo phpScriptInfo = isPhpRequest(httpRequest);
-		if (phpScriptInfo != null) {
-			String pathInfo = phpScriptInfo.getPathInfo();
-			if (!StringUtils.isEmpty(pathInfo)) {
-				request.setAttribute(Constants.REQ_ATTR_PATH_INFO, pathInfo);
-			}
-			RequestDispatcher dispatcher = servletContext
-					.getNamedDispatcher(quercusServletName);
-			// RequestDispatcher dispatcher = servletContext
-			// .getRequestDispatcher(phpScriptInfo.getScriptPath());
-			dispatcher.forward(request, response);
-		} else {
-			chain.doFilter(request, response);
-		}
-		return;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        PhpScriptInfo phpScriptInfo = isPhpRequest(httpRequest);
+        if (phpScriptInfo != null) {
+            String pathInfo = phpScriptInfo.getPathInfo();
+            if (!StringUtils.isEmpty(pathInfo)) {
+                request.setAttribute(Constants.REQ_ATTR_PATH_INFO, pathInfo);
+            }
+            RequestDispatcher dispatcher = servletContext.getNamedDispatcher(quercusServletName);
+            // RequestDispatcher dispatcher =
+            // servletContext.getRequestDispatcher(phpScriptInfo
+            // .getScriptPath());
+            dispatcher.forward(request, response);
+        } else {
+            chain.doFilter(request, response);
+        }
+        return;
+    }
 }
--- src/org/ddth/phpservlet/PhpServlet.java
+++ src/org/ddth/phpservlet/PhpServlet.java
@@ -13,57 +13,48 @@ import com.caucho.quercus.servlet.QuercusServlet;
 
 public class PhpServlet extends QuercusServlet {
 
-	private static final long serialVersionUID = 1L;
-	private String contextPath;
+    private static final long serialVersionUID = 1L;
+    private String contextPath;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void init() throws ServletException {
-		contextPath = getServletContext().getContextPath();
-		super.init();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void init() throws ServletException {
+        contextPath = getServletContext().getContextPath();
+        super.init();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		Object pathInfo = request.getAttribute(Constants.REQ_ATTR_PATH_INFO);
-		if (pathInfo != null) {
-			String requestUri = request.getRequestURI();
-			String servletPath = StringUtils.substringBeforeLast(StringUtils
-					.substringAfter(requestUri, contextPath), pathInfo
-					.toString());
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Object pathInfo = request.getAttribute(Constants.REQ_ATTR_PATH_INFO);
+        if (pathInfo != null) {
+            String requestUri = request.getRequestURI();
+            String servletPath = StringUtils.substringBeforeLast(StringUtils.substringAfter(
+                    requestUri, contextPath), pathInfo.toString());
 
-			request.setAttribute(RequestDispatcher.FORWARD_CONTEXT_PATH,
-					contextPath);
-			request.setAttribute(RequestDispatcher.INCLUDE_CONTEXT_PATH,
-					contextPath);
+            request.setAttribute(RequestDispatcher.FORWARD_CONTEXT_PATH, contextPath);
+            request.setAttribute(RequestDispatcher.INCLUDE_CONTEXT_PATH, contextPath);
 
-			request.setAttribute(RequestDispatcher.FORWARD_PATH_INFO, pathInfo);
-			request.setAttribute(RequestDispatcher.INCLUDE_PATH_INFO, pathInfo);
+            request.setAttribute(RequestDispatcher.FORWARD_PATH_INFO, pathInfo);
+            request.setAttribute(RequestDispatcher.INCLUDE_PATH_INFO, pathInfo);
 
-			String queryString = request.getQueryString();
-			if (!StringUtils.isEmpty(queryString)) {
-				request.setAttribute(RequestDispatcher.FORWARD_QUERY_STRING,
-						requestUri);
-				request.setAttribute(RequestDispatcher.INCLUDE_QUERY_STRING,
-						requestUri);
-			}
+            String queryString = request.getQueryString();
+            if (!StringUtils.isEmpty(queryString)) {
+                request.setAttribute(RequestDispatcher.FORWARD_QUERY_STRING, requestUri);
+                request.setAttribute(RequestDispatcher.INCLUDE_QUERY_STRING, requestUri);
+            }
 
-			request.setAttribute(RequestDispatcher.FORWARD_REQUEST_URI,
-					requestUri);
-			request.setAttribute(RequestDispatcher.INCLUDE_REQUEST_URI,
-					requestUri);
+            request.setAttribute(RequestDispatcher.FORWARD_REQUEST_URI, requestUri);
+            request.setAttribute(RequestDispatcher.INCLUDE_REQUEST_URI, requestUri);
 
-			request.setAttribute(RequestDispatcher.FORWARD_SERVLET_PATH,
-					servletPath);
-			request.setAttribute(RequestDispatcher.INCLUDE_SERVLET_PATH,
-					servletPath);
-		}
-		super.service(request, response);
-	}
+            request.setAttribute(RequestDispatcher.FORWARD_SERVLET_PATH, servletPath);
+            request.setAttribute(RequestDispatcher.INCLUDE_SERVLET_PATH, servletPath);
+        }
+        super.service(request, response);
+    }
 }
